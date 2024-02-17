a0cea556378f7eab553c1de0b943d578e09f9217
Jane Marimo <jenny.bulatova@gmail.com>
2024-02-17 21:23:01 +0300

    Фу

--- src/js/GameLogic.js
+++ src/js/GameLogic.js
@@ -1,56 +1,132 @@
-export default class Game {
-  constructor() {
-    this.gameContainer = document.querySelector('.game-container');
-    this.initializeGame();
-  }
+import Goblin from './Goblin'
+import CellRenderer from './CellRenderer'
 
-  generateGameField() {
-    for (let row = 0; row < 4; row++) {
-      for (let col = 0; col < 4; col++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        this.gameContainer.appendChild(cell);
-      }
+export default class GameLogic {
+    constructor() {
+        this.gameContainer = document.querySelector('.game-container')
+        this.hitCounter = document.querySelector('.hit')
+        this.lostCounter = document.querySelector('.lost')
+        this.restartButton = null
+        this.intervalId = null
+        this.gameOver = false
+        this.score = 0
+        this.prevCell = 0
+        this.missCount = 0
+        this.cells = []
+        this.goblin = new Goblin()
+
+        this.generateGameField()
+        this.moveGoblin()
+        this.handleGoblinClick()
     }
-  }
 
-  generateRandomPosition() {
-    const row = Math.floor(Math.random() * 4) + 1;
-    const col = Math.floor(Math.random() * 4) + 1;
-    return { row, col };
-  }
+    generateGameField() {
+        for (let i = 0; i < 16; i++) {
+            const cell = new CellRenderer()
+            this.cells.push(cell)
+            this.gameContainer.appendChild(cell.element)
+        }
+    }
 
-  createGoblin() {
-    const goblin = document.createElement('img');
-    goblin.src = './img/goblin.png';
-    goblin.alt = 'Goblin';
-    goblin.classList.add('game-goblin');
-    const randomPosition = this.generateRandomPosition();
-    goblin.dataset.row = randomPosition.row;
-    goblin.dataset.col = randomPosition.col;
-    this.gameContainer.appendChild(goblin);
-  }
+    generateRandomPosition() {
+        if (this.cells.length === 1) {
+            return this.cells[0]
+        }
 
-  handleGoblinClick() {
-    const goblin = document.querySelector('.game-goblin');
-    const randomPosition = this.generateRandomPosition();
-    goblin.dataset.row = randomPosition.row;
-    goblin.dataset.col = randomPosition.col;
-    this.updateGoblinPosition();
-  }
+        let randomCell
+        do {
+            randomCell = Math.floor(Math.random() * this.cells.length)
+        } while (this.prevCell === randomCell)
 
-  updateGoblinPosition() {
-    const goblin = document.querySelector('.game-goblin');
-    const { row } = goblin.dataset;
-    const { col } = goblin.dataset;
-    const newPosition = `grid-column: ${col} / span 1; grid-row: ${row} / span 1;`;
-    goblin.style.cssText = newPosition;
-  }
+        this.prevCell = randomCell
+        return this.cells[randomCell].element
+    }
 
-  initializeGame() {
-    this.generateGameField();
-    this.createGoblin();
-    this.handleGoblinClick();
-    setInterval(() => this.handleGoblinClick(), 1000);
-  }
+    moveGoblin() {
+        this.placeGoblin()
+
+        this.intervalId = setInterval(() => {
+            this.placeGoblin()
+            this.goblin.element.classList.add('goblin-active')
+
+            if (this.missCount >= 5) {
+                this.endGame()
+            }
+        }, 1000)
+    }
+
+    placeGoblin() {
+        const randomCell = this.generateRandomPosition()
+        this.goblin.moveTo(randomCell)
+    }
+
+    handleGoblinClick() {
+        this.gameContainer.addEventListener('click', (event) => {
+            const goblinCell = event.target
+            if (goblinCell.classList.contains('goblin-active')) {
+                this.score++
+                this.hitCounter.textContent = `Попаданий: ${this.score}`
+                this.goblin.removeGoblinClass()
+            } else {
+                this.missCount++
+                this.lostCounter.textContent = `Промахов: ${this.missCount}`
+            }
+        })
+    }
+
+    endGame() {
+        clearInterval(this.intervalId)
+        if (this.hitCounter && this.lostCounter) {
+            this.hitCounter.textContent = `Игра окончена! Ваш счёт: ${this.score}`
+            this.lostCounter.textContent = ''
+        }
+
+        this.missCount = 0
+        this.score = 0
+        this.goblin.removeGoblinClass()
+        this.createRestartButton()
+        this.showRestartButton()
+        this.gameOver = true
+    }
+
+    createRestartButton() {
+        const scoreContainer = document.querySelector('.score')
+
+        if (!this.restartButton) {
+            this.restartButton = document.createElement('button')
+            this.restartButton.textContent = 'Начать заново'
+            this.restartButton.classList.add('restart-button')
+            this.restartButton.addEventListener('click', () => {
+                this.resetGame()
+            })
+
+            scoreContainer.appendChild(this.restartButton)
+        }
+    }
+
+    resetGame() {
+        this.gameOver = false
+        this.score = 0
+        this.prevCell = 0
+        this.missCount = 0
+
+        if (this.hitCounter && this.lostCounter) {
+            this.hitCounter.textContent = 'Попаданий: 0'
+            this.lostCounter.textContent = 'Промахов: 0'
+        }
+
+        this.goblin.removeGoblinClass()
+        this.placeGoblin()
+        this.moveGoblin()
+
+        this.hideRestartButton()
+    }
+
+    showRestartButton() {
+        this.restartButton.style.display = 'block'
+    }
+
+    hideRestartButton() {
+        this.restartButton.style.display = 'none'
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,65 @@
-const path = require('path');
-const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  target: 'web',
-  devtool: 'inline-source-map',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    historyApiFallback: true,
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
+    target: 'web',
+    devtool: 'inline-source-map',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
     },
-    open: true,
-    compress: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+    devServer: {
+        historyApiFallback: true,
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
         },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
+        open: true,
+        compress: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ],
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
         new CopyWebpackPlugin({
-      patterns: [
-        { from: 'src/img', to: 'img' },
-      ],
-    }),
-  ],
-};
+            patterns: [{ from: 'src/img', to: 'img' }],
+        }),
+    ],
+}
